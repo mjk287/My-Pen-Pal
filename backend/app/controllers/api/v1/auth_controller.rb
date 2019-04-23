e58c6011efbda1ea68b846b6f1d310d0968eee0d
mjk287 <myung.kim287@gmail.com>
2019-04-23 19:55:14 -0400

    initial setup with setting up user and penpal association. created and implemented auth controller with jwt token. created and implemented basic create, index methods in user controller. implemented signup login forms in their respective components

---
+++ backend/app/controllers/api/v1/auth_controller.rb
@@ -0,0 +1,23 @@
+class Api::V1::AuthController < ApplicationController
+  def create
+    @user = User.find_by(email: login_params[:email])
+    if @user && @user.authenticate(login_params[:password])
+      token = JWT.encode({ user_id: @user.id }, "secret")
+      render json: { user:@user, jwt: token }, status: :accepted
+    else
+      render json: { message: 'Invalid email or password' }, status: :unauthorized
+    end
+  end
+
+  def show
+    user_id = JWT.decode(request.headers["Authorization"], nil, false)[0]["user_id"]
+    user = User.find(user_id)
+    render json: { user: user }
+  end
+
+  private
+
+  def login_params
+    params.permit(:email, :password)
+  end
+end
