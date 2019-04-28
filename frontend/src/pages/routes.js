23d5b592560e44f7944219ee61d6e70f417ad1d3
mjk287 <myung.kim287@gmail.com>
2019-04-28 18:54:46 -0400

    implemented appear method in user model that will broadcast their presence to penpal stream. on the frontend, on receive of type 'appear' will change the current user's penpal's online status

--- frontend/src/pages/routes.js
+++ frontend/src/pages/routes.js
@@ -2,12 +2,30 @@ import React from 'react'
 import { Route, Switch, Redirect} from 'react-router-dom'
 import UserRoutes from './user/routes'
 import { connect } from 'react-redux'
+import { gotOnline } from '../Redux/actions'
 
 
 class Routes extends React.Component {
 
+  componentDidMount(){
+    const { currentUser } = this.props
+    const channel_ids = [currentUser.id, currentUser.my_penpal.id].sort()
+    this.props.cable.subscriptions.create({channel: 'PenpalChannel', room: `${currentUser.id}`}, {
+      connected: () => {},
+      disconnected: () => {},
+      received: (data) => {
+        switch(data.event){
+          case 'appear':
+            console.log('hit appear')
+            this.props.gotOnline()
+          default:
+            return null
+        }
+      }
+    })
+  }
+
   render(){
-    console.log(this.props.currentUser.my_penpal)
     return(
       <Switch>
         <Route
@@ -30,4 +48,10 @@ const mapStateToProps = (state) => {
   return state
 }
 
-export default connect(mapStateToProps)(Routes)
+const mapDispatchToProps = (dispatch) => {
+  return ({
+    gotOnline: () => dispatch(gotOnline())
+  })
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Routes)
