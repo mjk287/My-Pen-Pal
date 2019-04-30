2c25f3dfd69bd570dc9b5a090ad81c1a8e926149
github username <github email address>
2019-04-30 19:32:54 -0400

    styled the landing page including the login and signup forms

--- frontend/src/pages/landing/index.jsx
+++ frontend/src/pages/landing/index.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import Login from '../../components/Login'
 import Signup from '../../components/Signup'
+import { Grid, Container } from 'semantic-ui-react'
 
 class LandingPage extends React.Component {
   render(){
     return(
       <React.Fragment>
-        <Login />
-        <Signup />
+          <Grid verticalAlign='middle' centered relaxed divided id='landing-container'>
+            <Grid.Row id='landing-form-container'>
+              <Grid.Column width={8}>
+                <Login />
+              </Grid.Column>
+              <Grid.Column width={8}>
+                <Signup />
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+
       </React.Fragment>
     )
   }
