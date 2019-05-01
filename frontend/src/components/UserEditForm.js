dbbb5042b0166cd6d02c6d710b4d0123f36c9930
github username <github email address>
2019-05-01 09:01:35 -0400

    styled the edit page. added an image preview to the right of the segment

--- frontend/src/components/UserEditForm.js
+++ frontend/src/components/UserEditForm.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { Segment, Grid, Image, Icon, Button, Input } from 'semantic-ui-react'
 
 class UserEditForm extends React.Component {
 
@@ -8,7 +9,8 @@ class UserEditForm extends React.Component {
     password: '',
     passwordConfirmation: '',
     image: null,
-    song: null
+    song: null,
+    preview: null
   }
 
   changeHandler = (e) => {
@@ -18,33 +20,55 @@ class UserEditForm extends React.Component {
   }
 
   handleFileChange = (e) => {
-    this.setState({
-      [e.target.name]: e.target.files[0]
-    })
+    if(e.target.name === 'image'){
+      this.setState({
+        [e.target.name]: e.target.files[0],
+        preview: URL.createObjectURL(e.target.files[0])
+      })
+    } else {
+      this.setState({
+        [e.target.name]: e.target.files[0]
+      })
+    }
   }
 
   render(){
     return(
-      <form onSubmit = {(e) => this.props.submitHandler(e, this.state)}>
-        <label> Name:
-          <input type='text' name='name' value={this.state.name} placeholder={this.props.currentUser.name} onChange={this.changeHandler}/>
-        </label>
-        <label> Password:
-          <input type='password' name='password' value={this.state.password} onChange={this.changeHandler}/>
-        </label>
-        <label> Confirmation Password:
-          <input type='password' name='passwordConfirmation' value={this.state.passwordConfirmation} onChange={this.changeHandler}/>
-        </label>
-        <label> Upload Image:
-        <input type='file' name='image' onChange={this.handleFileChange}  />
-        </label>
-        <label> Upload Song:
-        <input type='file' name='song' onChange={this.handleFileChange}  />
-        </label>
-
-        <input type='submit' value='Submit Me!'/>
-
-      </form>
+      <Segment>
+        <Grid verticalAlign='middle' divided >
+        <Grid.Column width={8}>
+        <form onSubmit = {(e) => this.props.submitHandler(e, this.state)}>
+          <Grid.Row>
+            <input type='text' className='edit-form-input input-field-margin' name='name' value={this.state.name} placeholder={this.props.currentUser.name} onChange={this.changeHandler}/>
+          </Grid.Row>
+          <Grid.Row>
+            <input type='password' placeholder='password' className='edit-form-input input-field-margin' name='password' value={this.state.password} onChange={this.changeHandler}/>
+          </Grid.Row>
+          <Grid.Row>
+            <input type='password' placeholder='confirmation password' className='edit-form-input input-field-margin' name='passwordConfirmation' value={this.state.passwordConfirmation} onChange={this.changeHandler}/>
+          </Grid.Row>
+          <Grid.Row className='input-field-margin'>
+          <span ><Icon name='file image' />Upload a profile picture for yourself <label className='file-select-link'>here!
+          <input type='file' className='input-field-margin' name='image' onChange={this.handleFileChange}  />
+          </label></span>
+          </Grid.Row>
+          <Grid.Row className='input-field-margin'>
+          <span> <Icon name='music' />Upload a song for your penpal <label className='file-select-link'>here!
+          <input type='file' className='input-field-margin' name='song' onChange={this.handleFileChange}  />
+          </label></span>
+          </Grid.Row>
+          <Grid.Row>
+          <Input type='submit' className='input-field-margin' value='Submit Me!'/>
+          </Grid.Row>
+        </form>
+        </Grid.Column>
+        <Grid.Column width={8}>
+          {!!this.state.preview &&
+            <Image className='our-profile-imgs' src={this.state.preview} centered/>
+          }
+        </Grid.Column>
+        </Grid>
+      </Segment>
     )
   }
 
