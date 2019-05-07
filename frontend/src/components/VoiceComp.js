266aaf355668ad3ec79c6772446238cff49c5bf2
mjk287 <myung.kim287@gmail.com>
2019-05-07 15:41:00 -0400

    added a voice-recorder beep at the beginning and end to simulate one. set a timeout for the actual audio content. removed feedback due to echo

--- frontend/src/components/VoiceComp.js
+++ frontend/src/components/VoiceComp.js
@@ -5,6 +5,7 @@ class VoiceComp extends React.Component{
   constructor(props){
     super(props)
     this.audioTag = React.createRef()
+    this.beepTag = React.createRef()
   }
 
   state = {
@@ -13,14 +14,18 @@ class VoiceComp extends React.Component{
 
   playHandler = (e) => {
     e.preventDefault()
+    console.log(this.beepTag.current.paused)
+    this.beepTag.current.play()
 
     this.setState({
       playing: true
-    }, () => this.audioTag.current.play())
+    }, () => setTimeout(() => this.audioTag.current.play(), 1000))
   }
 
   stopHandler = (e) => {
     e.preventDefault()
+
+    this.beepTag.current.play()
 
     this.setState({
       playing: false
@@ -28,6 +33,14 @@ class VoiceComp extends React.Component{
       this.audioTag.current.pause();
       this.audioTag.current.currentTime = 0
     })
+  }
+
+  componentDidMount(){
+    this.beepTag.current.crossOrigin = "anonymous"
+    let audioContext = new AudioContext()
+    const track = audioContext.createMediaElementSource(this.beepTag.current)
+    const filterNode = audioContext.createIIRFilter([0.00020298, 0.0004059599, 0.00020298], [1.0126964558, -1.9991880801, 0.9873035442])
+    track.connect(filterNode).connect(audioContext.destination)
   }
 
   render(){
@@ -38,6 +51,7 @@ class VoiceComp extends React.Component{
           :
           <Button icon='play circle outline' content=' Listen To Voicemail' onClick={this.playHandler} className='voice-button'/> }
         <audio ref={this.audioTag} src={`http://localhost:3000/${this.props.voice}`}/>
+        <audio ref={this.beepTag} src={require('../assets/beep.mp3')}/>
       </React.Fragment>
     )
   }
