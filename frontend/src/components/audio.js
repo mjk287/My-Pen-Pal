c7087cb412025870353a9f9878f738af277e8a72
mjk287 <myung.kim287@gmail.com>
2019-05-09 14:23:39 -0400

    changed buttons in signup and login page. now 'plays' penpal song rather than being set to autoplay

--- frontend/src/components/audio.js
+++ frontend/src/components/audio.js
@@ -27,15 +27,13 @@ class AudioComp extends React.Component {
     // [0.00020298, 0.0004059599, 0.00020298],
 		// feedback: [1.0126964558, -1.9991880801, 0.9873035442]
     track.connect(filterNode).connect(audioContext.destination)
-
-
-
+    this.audioTag.current.play()
   }
 
   render(){
     return(
       <React.Fragment>
-        <audio src={`http://localhost:3000${this.props.currentUser.my_penpal.song}`} autoPlay ref={this.audioTag}></audio>
+        <audio src={`http://localhost:3000${this.props.currentUser.my_penpal.song}`} ref={this.audioTag}></audio>
 
       </React.Fragment>
     )
